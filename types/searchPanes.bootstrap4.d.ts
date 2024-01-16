9c1f23e5bc5b9f2da0bf0209526a4496929c7b95
allan@datatables.net <dtbuild@rigil>
2024-01-16 17:06:04 +0100

    0520bde1e3544ebd1545ade211c2f4495b2be620
    Sync to source repo @d838ad9bc15f6751bac8d651cf0d10816d7ed70b

--- types/searchPanes.bootstrap4.d.ts
+++ types/searchPanes.bootstrap4.d.ts
@@ -1,4 +1,5 @@
-/*! Bootstrap integration for DataTables' SearchPanes
- * © SpryMedia Ltd - datatables.net/license
- */
-declare var DataTable: any;
+
+import DataTable from 'datatables.net-searchpanes';
+
+export default DataTable;
+export * from 'datatables.net-searchpanes';
