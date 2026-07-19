d15771772313573579c7c900ffd8b97b2fbabca7
dtbuild <dtbuild@rigil>
2026-07-19 14:00:15 +0000

    ff1d8320e468eaccb95624a56f2f1300b9c41923 Examples: Buttons no longer needs extra files for the export buttons and column visibility.
    Sync to source repo @ff1d8320e468eaccb95624a56f2f1300b9c41923

--- js/searchPanes.bootstrap4.js
+++ js/searchPanes.bootstrap4.js
@@ -1,57 +1,6 @@
 /*! Bootstrap integration for DataTables' SearchPanes
  * © SpryMedia Ltd - datatables.net/license
  */
-
-(function( factory ){
-	if ( typeof define === 'function' && define.amd ) {
-		// AMD
-		define( ['jquery', 'datatables.net-bs4', 'datatables.net-searchpanes'], function ( $ ) {
-			return factory( $, window, document );
-		} );
-	}
-	else if ( typeof exports === 'object' ) {
-		// CommonJS
-		var jq = require('jquery');
-		var cjsRequires = function (root, $) {
-			if ( ! $.fn.dataTable ) {
-				require('datatables.net-bs4')(root, $);
-			}
-
-			if ( ! $.fn.dataTable.SearchPanes ) {
-				require('datatables.net-searchpanes')(root, $);
-			}
-		};
-
-		if (typeof window === 'undefined') {
-			module.exports = function (root, $) {
-				if ( ! root ) {
-					// CommonJS environments without a window global must pass a
-					// root. This will give an error otherwise
-					root = window;
-				}
-
-				if ( ! $ ) {
-					$ = jq( root );
-				}
-
-				cjsRequires( root, $ );
-				return factory( $, root, root.document );
-			};
-		}
-		else {
-			cjsRequires( window, jq );
-			module.exports = factory( jq, window, window.document );
-		}
-	}
-	else {
-		// Browser
-		factory( jQuery, window, document );
-	}
-}(function( $, window, document ) {
-'use strict';
-var DataTable = $.fn.dataTable;
-
-
 $.extend(true, DataTable.SearchPane.classes, {
     buttonGroup: 'btn-group',
     disabledButton: 'disabled',
@@ -80,7 +29,3 @@ $.extend(true, DataTable.SearchPanes.classes, {
     title: 'dtsp-title',
     titleRow: 'dtsp-titleRow'
 });
-
-
-return DataTable;
-}));
